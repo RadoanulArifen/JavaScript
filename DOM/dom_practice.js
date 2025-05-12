a1b5182b14ef87b44fdc206324d983b55ca506ec
Arifen <113052888+ArifenFahim@users.noreply.github.com>
2025-05-12 23:54:57 +0600

    DOM

---
+++ DOM/dom_practice.js
@@ -0,0 +1,15 @@
+const marry = ["a","b","c",];
+console.log( marry)
+console.log( marry[2])
+marry.push("Arifen")
+console.log(marry)
+
+
+
+const item = ["a","b","c","d","e"];
+console.log(item)
+function tell(item){
+    return item + "____";
+}
+const item_tell = item.map(tell)
+console.log(item_tell)
