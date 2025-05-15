62404771854046ec7f5040df9506202438c258c6
Arifen <113052888+ArifenFahim@users.noreply.github.com>
2025-05-15 15:30:19 +0600

    Advance

---
+++ advance.js
@@ -0,0 +1,11 @@
+const name = "Arifen";
+console.log(`Hello ${name}`);
+
+ function anisa(){
+    return `I love you ${name}`;
+ }
+console.log(anisa());
+
+let numbers = [1,2,3];
+let newnumbers = [...numbers,5]
+console.log(newnumbers);
