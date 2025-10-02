4787d319f457b065affd467a68cbc47fd40e99c3
Arifen <113052888+ArifenFahim@users.noreply.github.com>
2025-10-02 23:52:01 +0600

    Non Primative Data Type

---
+++ Reeevised For SQA/Non Primative Data Type.js
@@ -0,0 +1,22 @@
+
+//object
+const druvo = {
+    name:"Arifin",
+    age:20,
+    isPass: true
+};
+console.log(druvo.name);
+console.log(druvo.age);
+
+//array
+const name = ["Arifen","Rupom","Rafi","Anik"]
+console.log(name[1]);
+
+
+//function
+function greatting(){
+    console.log("Good Morning");
+    
+}
+
+greatting();
